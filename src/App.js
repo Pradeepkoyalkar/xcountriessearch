9d7534ff85fbcea31f6745151c8ca67663b0600a
Pradeep Koyalkar <koyalkarpradeep44@gmail.com>
2026-05-25 10:48:55 +0530

    countries search app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React,
+{
+  useEffect,
+  useState
+}
+from "react";
+
+import "./App.css";
 
 function App() {
+
+  const [countries, setCountries] =
+    useState([]);
+
+  const [search, setSearch] =
+    useState("");
+
+  // FETCH API
+
+  useEffect(() => {
+
+    fetch(
+      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+    )
+
+      .then((response) =>
+        response.json()
+      )
+
+      .then((data) => {
+
+        setCountries(data);
+      })
+
+      .catch((error) => {
+
+        console.error(error);
+      });
+
+  }, []);
+
+  // FILTER COUNTRIES
+
+  const filteredCountries =
+    countries.filter(
+      (country) =>
+
+        country.common
+          .toLowerCase()
+          .includes(
+            search.toLowerCase()
+          )
+    );
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* SEARCH BAR */}
+
+      <input
+        type="text"
+        placeholder="Search for countries..."
+        value={search}
+        onChange={(e) =>
+          setSearch(
+            e.target.value
+          )
+        }
+      />
+
+      {/* COUNTRY CARDS */}
+
+      <div className="countries-container">
+
+        {filteredCountries.map(
+          (country) => (
+
+            <div
+              className="countryCard"
+              key={country.common}
+            >
+
+              <img
+                src={country.png}
+                alt={
+                  country.common
+                }
+              />
+
+              <h3>
+                {country.common}
+              </h3>
+
+            </div>
+          )
+        )}
+
+      </div>
+
     </div>
   );
 }
